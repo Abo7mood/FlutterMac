1e4c6255074ff7955b0c6cc6a3cf37f217eca55d
Nasser Almuqbil <nasseralmuqbil@Nassers-MacBook-Air.local>
2022-08-24 20:05:58 +0300

    d 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,48 @@
-
 import 'package:flutter/material.dart';
 
-
-class A{
-void main(){
-runApp(B());
+void main() {
+  runApp(B());
 }
-
-}
-
 
 class B extends StatelessWidget {
-int x=5;
-  Widget build(BuildContext buildContext) {
-    return MaterialApp(home: Text("Hi :"+x.toString()),);
-  }
+  /// here we have some varibles
 
+  //questions list
+  List<String> questions = [
+    'are you dumb?',
+    'what is your name',
+    'how old r u?'
+  ];
+
+  //function
+  void onPressed() {}
+  @override
+  Widget build(BuildContext buildContext) {
+    return MaterialApp(
+      color: Color.fromARGB(255, 36, 15, 138),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 125, 29, 29),
+          title: Text('title'),
+        ),
+        body: Column(
+          children: [
+            Text('question'),
+            ElevatedButton(
+              onPressed: () => print('1'),
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('2'),
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('3'),
+              child: Text('Answer 3'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
